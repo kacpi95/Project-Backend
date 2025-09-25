41e58725668a8b5c21a63bb4171d3b346b8fb487
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-09-25 19:44:31 +0200

    immport routes

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const adsRoutes = require('./routes/ads.routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use('/api', adsRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/build')));
 app.get('*', (req, res) => {
