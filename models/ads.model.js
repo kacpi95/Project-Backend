31e8d96413a2e5619dcdf17f1ca4cecd986c3d17
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-09-25 19:01:57 +0200

    add: mongoose Schema

---
+++ models/ads.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const adsSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  text: { type: String, required: true },
+  date: { type: Number, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  location: { type: String, required: true },
+  aboutSeller: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Ads', adsSchema);
