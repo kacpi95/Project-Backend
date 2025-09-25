0a0753ce2d86ec31e0731a84d98ba684f346ed9e
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-09-25 18:29:35 +0200

    add: server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: '404 not found...' });
+});
+
+const server = app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
+});
