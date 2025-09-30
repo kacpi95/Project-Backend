c915db62fa0ec168f593c6cb9c9cc2110924b72b
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-09-30 13:31:43 +0200

    fix: ads.routes

--- routes/ads.routes.js
+++ routes/ads.routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const AdsController = require('../controllers/ads.controller');
+const imageUpload = require('../utils/imageUpload');
 
-router.get('/ads', AdsController.getAll);
-router.get('/ads/:id', AdsController.getAdsId);
-router.post('/ads', AdsController.postAds);
-router.delete('/ads/:id', AdsController.deleteAds);
-router.put('/ads/:id', AdsController.putAds);
-router.get('/ads/search/:searchPhrase', AdsController.getSearchAds);
+router.get('/', AdsController.getAll);
+router.get('/:id', AdsController.getAdsId);
+router.post('/', imageUpload.single('image'), AdsController.postAds);
+router.delete('/:id', AdsController.deleteAds);
+router.put('/:id', imageUpload.single('image'), AdsController.putAds);
+router.get('/search/:searchPhrase', AdsController.getSearchAds);
 
 module.exports = router;
