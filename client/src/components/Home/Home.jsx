e252dc4bda1d417aa832b7c8a8856563a66752ee
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-10-02 23:10:12 +0200

    add: Home styles and update global.css

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,20 +1,25 @@
 import styles from './Home.module.css';
 import { Link } from 'react-router-dom';
+import logo from './kacpiAI.jpg';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <form>
-        <input type='text' placeholder='Search ...' />
+      <form className={styles.searchForm}>
+        <input
+          className={styles.searchInput}
+          type='text'
+          placeholder='Search ...'
+        />
         <button type='submit' className={styles.searchButton}>
           Search
         </button>
       </form>
       <div className={styles.grid}>
         <div className={styles.card}>
-          <img src='' alt='' className={styles.image} />
-          <h3 className={styles.title}>x</h3>
-          <p className={styles.location}></p>
+          <img src={logo} alt='' className={styles.image} />
+          <h3 className={styles.title}>title</h3>
+          <p className={styles.location}>location</p>
           <Link className={styles.readMore}>Read More</Link>
         </div>
       </div>
