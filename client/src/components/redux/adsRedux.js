8b03e47f8a1b23d0e8fd8424df256c5db4bbe137
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-10-05 19:29:06 +0200

    add: createSlice in Redux

--- client/src/components/redux/adsRedux.js
+++ client/src/components/redux/adsRedux.js
@@ -28,3 +28,46 @@ export const updateAd = createAsyncThunk(
     return res.data;
   }
 );
+
+const adsSlice = createSlice({
+  name: 'ads',
+  initialState: {
+    list: [],
+    currentAd: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchAds.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(fetchAds.fulfilled, (state, action) => {
+      state.loading = false;
+      state.list = action.payload;
+    });
+    builder.addCase(fetchAds.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.error.message;
+    });
+
+    builder.addCase(fetchAdId.fulfilled, (state, action) => {
+      state.currentAd = action.payload;
+    });
+    builder.addCase(addAd.fulfilled, (state, action) => {
+      state.list.push(action.payload);
+    });
+    builder.addCase(deleteAd.fulfilled, (state, action) => {
+      state.list = state.list.filter((ad) => ad._id !== action.payload);
+    });
+    builder.addCase(updateAd.fulfilled, (state, action) => {
+      const index = state.list.findIndex((ad) => ad._id === action.payload._id);
+      if (index !== -1) {
+        state.list[index] = action.payload;
+      }
+    });
+  },
+});
+
+export default adsSlice.reducer;
