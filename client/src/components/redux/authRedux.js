501ac7d4b8408ea6aac5ebd185c3b4caee310cf9
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-10-06 16:47:09 +0200

    add: reducer for login,register,user

---
+++ client/src/components/redux/authRedux.js
@@ -0,0 +1,61 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000/auth';
+
+export const login = createAsyncThunk('auth/login', async (loginData) => {
+  const res = await axios.post(`${API_URL}/login`, loginData);
+  return res.data;
+});
+
+export const register = createAsyncThunk('auth/register', async (regiData) => {
+  const res = await axios.post(`${API_URL}/register`, regiData);
+  return res.data;
+});
+export const fetchUser = createAsyncThunk('auth/user', async () => {
+  const res = await axios.get(`${API_URL}/user`, { withCredentials: true });
+  return res.data;
+});
+
+export const logout = createAsyncThunk('auth/logout', async () => {
+  const res = await axios.delete(`${API_URL}/logout`, {
+    withCredentials: true,
+  });
+  return res.data;
+});
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    user: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.user = action.payload;
+      })
+      .addCase(fetchUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+      })
+      .addCase(logout.fulfilled, (state) => {
+        state.user = null;
+      });
+  },
+});
+
+export default authSlice.reducer;
--- client/src/components/redux/store.js
+++ client/src/components/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import adsRedux from './adsRedux';
+import authRedux from './authRedux';
 
 const store = configureStore({
   reducer: {
     ads: adsRedux,
+    auth: authRedux,
   },
 });
 
