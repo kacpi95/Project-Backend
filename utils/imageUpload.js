594a0d612b2fe0a2c5a57a3fec06548a578b7d60
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-10-10 18:44:28 +0200

    fix image add

--- utils/imageUpload.js
+++ utils/imageUpload.js
@@ -1,17 +1,15 @@
 const multer = require('multer');
-const path = require('path');
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '..', 'public', 'uploads'));
+  destination: (req, res, cb) => {
+    cb(null, './client/public/uploads');
   },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-    const extension = path.extname(file.originalname);
-    cb(null, file.fieldname + '-' + uniqueSuffix + extension);
+    const [name, ext] = file.originalname.split('.');
+    cb(null, `${name}-${Date.now()}.${ext}`);
   },
 });
 
-const imageUpload = multer({ storage: storage });
+const imageUpload = multer({ storage, limits: { fileSize: 1024 * 1024 * 2 } });
 
 module.exports = imageUpload;
